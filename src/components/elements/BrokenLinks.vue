3250a0df532810b48affad4fea1320d07515bc2d
Bogdan Jovanovic <bogdanjovanovic@cloud.upwork.com>
2022-12-11 01:33:58 +0100

    Bind src attribute

--- src/components/elements/BrokenLinks.vue
+++ src/components/elements/BrokenLinks.vue
@@ -5,10 +5,10 @@
 
 	<div class="row mt-5">
 		<div class="col">
-			<img src="../../assets/rickroll.jpg" class="img-thumbnail" width="200" height="200" />
+			<img :src="'src/assets/rickroll.jpg'" class="img-thumbnail" width="200" height="200" />
 		</div>
 		<div class="col">
-			<img src="../../assets/rickrolled.jpg" class="img-thumbnail" alt="Broken Image" />
+			<img :src="'src/assets/rickrolled.jpg'" class="img-thumbnail" alt="Broken Image" />
 			<!-- <a href="https://the-internet.herokuapp.com/broken_images">Broken Images</a> -->
 		</div>
 		<div class="col">
